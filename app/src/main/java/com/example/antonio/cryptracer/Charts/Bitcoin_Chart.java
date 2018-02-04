4330d71afcdd2cf54896df01598864d0a9470043
Antonio <antonio7.9@hotmail.com>
2018-02-04 18:23:22 +0000

    Daily chart finished

--- app/src/main/java/com/example/antonio/cryptracer/Charts/Bitcoin_Chart.java
+++ app/src/main/java/com/example/antonio/cryptracer/Charts/Bitcoin_Chart.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.antonio.cryptracer.R;
 import com.github.mikephil.charting.charts.CandleStickChart;
@@ -11,33 +12,89 @@ import com.github.mikephil.charting.data.CandleData;
 import com.github.mikephil.charting.data.CandleDataSet;
 import com.github.mikephil.charting.data.CandleEntry;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 import java.util.ArrayList;
 
 public class Bitcoin_Chart extends AppCompatActivity {
+
+    public static final String API_CALL = "https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=365&aggregate=3&e=CCCAGG";
+    OkHttpClient okHttpClient = new OkHttpClient();
+    private CandleStickChart candleStickChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bitcoin_chart);
-        CandleStickChart candleStickChart = (CandleStickChart) findViewById(R.id.chart);
-        ArrayList<CandleEntry> entries = new ArrayList<>();
-        entries.add(new CandleEntry(0, 4.62f, 2.02f, 2.70f, 4.13f));
-        entries.add(new CandleEntry(1, 5.50f, 2.70f, 3.35f, 4.96f));
-        entries.add(new CandleEntry(2, 5.25f, 3.02f, 3.50f, 4.50f));
-        entries.add(new CandleEntry(3, 6f,    3.25f, 4.40f, 5.0f));
-        entries.add(new CandleEntry(4, 5.57f, 2f,    2.80f, 4.5f));
-        CandleDataSet cds = new CandleDataSet(entries, "Entries");
-        cds.setColor(Color.rgb(80, 80, 80));
-        cds.setShadowColor(Color.DKGRAY);
-        cds.setShadowWidth(0.7f);
-        cds.setDecreasingColor(Color.DKGRAY);
-        cds.setDecreasingPaintStyle(Paint.Style.FILL);
-        cds.setIncreasingColor(Color.BLUE);
-        cds.setIncreasingPaintStyle(Paint.Style.FILL);
-        cds.setNeutralColor(Color.BLUE);
-        cds.setValueTextColor(Color.BLACK);
-        CandleData cd = new CandleData(cds);
-        candleStickChart.setData(cd);
-        candleStickChart.invalidate();
+        candleStickChart = (CandleStickChart) findViewById(R.id.chart);
+        run();
+    }
+
+    private void run(){
+        Request request = new Request.Builder()
+                .url(API_CALL)
+                .build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Toast.makeText(Bitcoin_Chart.this, "Error during BPI loading : "
+                        + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response)
+                    throws IOException {
+                final String body = response.body().string();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        parseBpiResponse(body);
+                    }
+                });
+            }
+        });
+    }
+
+    private void parseBpiResponse(String body){
+        try{
+            candleStickChart = (CandleStickChart) findViewById(R.id.chart);
+            ArrayList<CandleEntry> entries = new ArrayList<>();
+            JSONObject jsonResponse = new JSONObject(body);
+            JSONArray CandleStickEntries = jsonResponse.getJSONArray("Data");
+            for(int i = 0; i < CandleStickEntries.length(); i++){
+                JSONObject CandleStickEntry = CandleStickEntries.getJSONObject(i);
+                float close = (float) CandleStickEntry.getDouble("close");
+                float high = (float) CandleStickEntry.getDouble("high");
+                float low = (float) CandleStickEntry.getDouble("low");
+                float open = (float) CandleStickEntry.getDouble("open");
+                entries.add(new CandleEntry(i, high, low, open, close));
+            }
+            CandleDataSet cds = new CandleDataSet(entries, "Daily entries for the last 365 days");
+            cds.setColor(Color.rgb(80, 80, 80));
+            cds.setShadowColor(Color.DKGRAY);
+            cds.setShadowWidth(0.7f);
+            cds.setDecreasingColor(Color.RED);
+            cds.setDecreasingPaintStyle(Paint.Style.FILL);
+            cds.setIncreasingColor(Color.GREEN);
+            cds.setIncreasingPaintStyle(Paint.Style.FILL);
+            cds.setNeutralColor(Color.GREEN);
+            cds.setValueTextColor(Color.BLACK);
+            CandleData cd = new CandleData(cds);
+            candleStickChart.setData(cd);
+            candleStickChart.invalidate();
+
+        } catch (Exception e){
+
+        }
     }
 }
